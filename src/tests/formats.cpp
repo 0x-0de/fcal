630140dfae9c0085c7d15458d1ddf3baa30bc770
0x0de <p_0de-music@outlook.com>
2023-12-12 12:11:55 -0500

    Updated tests/formats.cpp with additional sample rate conversion tests.

--- src/tests/formats.cpp
+++ src/tests/formats.cpp
@@ -14,11 +14,16 @@ int main()
     fcal::audio_stream mono24("resources/jingle 24bit mono.wav");
     fcal::audio_stream mono32("resources/jingle 32bit mono.wav");
 
+    fcal::audio_stream khz16("resources/jingle 16khz.wav");
+    fcal::audio_stream khz96("resources/jingle 96khz.wav");
+
     fcal::audio_source source;
 
     fcal::register_source(&source);
 
-    std::cout << "Testing .wav playback with different formats..." << std::endl;
+    std::cout << "Testing .wav playback with different formats...\n" << std::endl;
+    std::cout << "If the audio appears to glitch at the end of playback after 32-bit stereo,\nit's likely a problem with the exported .wav files I'm using.\n" << std::endl;
+    std::cout << "All 44.1 khz.\n" << std::endl;
 
     std::cout << "Playing: 16-bit stereo." << std::endl;
     source.play(&stereo16);
@@ -42,6 +47,14 @@ int main()
 
     std::cout << "Playing: 32-bit mono." << std::endl;
     source.play(&mono32);
+    Sleep(1500);
+
+    std::cout << "Playing: 16khz. (32-bit stereo)" << std::endl;
+    source.play(&khz16);
+    Sleep(1500);
+
+    std::cout << "Playing: 96khz. (32-bit stereo)" << std::endl;
+    source.play(&khz96);
     Sleep(1500);
 
     std::cout << "Test concluded." << std::endl;
